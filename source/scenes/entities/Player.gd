16c4d6c7a761556cfc4f79b1fd3234adf3a6ba51
mrdudeiii <josiah.enns@gmail.com>
2019-04-05 14:09:33 -0400

    added character to engine, added pushing animation, refactored code in player.gd. changed to walking push rather than button push for block movement.

--- source/scenes/entities/Player.gd
+++ source/scenes/entities/Player.gd
@@ -2,9 +2,12 @@ extends KinematicBody
 
 # Y-component is zero so we stay flat on the ground
 export var move_speed:int = 200 # pixels per second?
+export var block_push_delay:float = 0.2 # in seconds, how long before the block pushes on contact
 var _acceleration:Vector3 = Vector3(0, 0, 0)
 var can_move = true
-const RAYCAST_DISTANCE = 1.25
+var _push_delay = 0.0 
+var _last_input:Vector2 = Vector2(0, 0) 
+const RAYCAST_DISTANCE = 1.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,35 +16,49 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if can_move:
-		if Input.is_key_pressed(KEY_S):
-			self._acceleration.z = move_speed
-			$RayCast.cast_to = Vector3(0, 0, RAYCAST_DISTANCE)
-		elif Input.is_key_pressed(KEY_W):
-			self._acceleration.z = -move_speed
-			$RayCast.cast_to = Vector3(0, 0, -RAYCAST_DISTANCE)
+		# MrDudeIII found this in a KidsCanCode tutorial
+		var input_direction = Vector2(
+			int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
+			int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+			)
+		if not input_direction:
+			_push_delay = 0.0 # reset the push delay if you stop pressing a button
+			$PlayerCharacter/AnimationPlayer.play("Stand")
+			return
 		else:
-			self._acceleration.z = 0
+			input_direction = input_direction.normalized()
+			
 		
-		if Input.is_key_pressed(KEY_D):
-			$RayCast.cast_to = Vector3(RAYCAST_DISTANCE, 0, 0)
-			self._acceleration.x = move_speed
-		elif Input.is_key_pressed(KEY_A):
-			self._acceleration.x = -move_speed
-			$RayCast.cast_to = Vector3(-RAYCAST_DISTANCE, 0, 0)
+		_acceleration = Vector3(input_direction.x, 0, input_direction.y) * move_speed
+		if abs(input_direction.x) == 1.0 or abs(input_direction.y) == 1.0:
+			$RayCast.cast_to = Vector3(input_direction.x, 0, input_direction.y) * RAYCAST_DISTANCE
+			$RayCastFar.cast_to = $RayCast.cast_to * 4
+		var collider = $RayCast.get_collider()
+		
+		if collider != null:
+			$RayCastFar.enabled = true
+			$RayCastFar.add_exception(collider)
+			var behind_collider = $RayCastFar.get_collider()
+			_push_delay += delta
+			if collider.pushable and behind_collider == null and _push_delay >= block_push_delay and _last_input == input_direction: # added a delay instead of a button press
+#				$Tween.reset_all()
+				$Tween.interpolate_property(collider, "translation", collider.translation, collider.translation + $RayCast.cast_to * 2, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+				$Tween.interpolate_property(self, "translation", self.translation, self.translation + $RayCast.cast_to * 2, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+				$Tween.start()
+				can_move = false
+			$PlayerCharacter/AnimationPlayer.play("Push")
+			$PlayerCharacter.rotation.y = Vector2(-$RayCast.cast_to.x, $RayCast.cast_to.z).tangent().angle()
+			
 		else:
-			self._acceleration.x = 0
-		
-		if Input.is_key_pressed(KEY_E):
-			var collider = $RayCast.get_collider()
-			if collider != null:
-				if collider.pushable:
-					$Tween.reset_all()
-					$Tween.interpolate_property(collider, "translation", collider.translation, collider.translation + $RayCast.cast_to * 2, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-					$Tween.interpolate_property(self, "translation", self.translation, self.translation + $RayCast.cast_to * 2, 1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-					$Tween.start()
-					can_move = false
-	
-	_acceleration = move_and_slide(self._acceleration * delta)
+			$RayCastFar.remove_exception(collider)
+			$RayCastFar.enabled = false
+			$Tween.reset_all()
+			_push_delay = 0.0
+			_acceleration = move_and_slide(self._acceleration * delta)
+			$PlayerCharacter/AnimationPlayer.play("Run")
+			$PlayerCharacter.rotation.y = -input_direction.tangent().angle()
+					
+		_last_input = input_direction
 
 func _on_Tween_tween_completed(object, key):
 	if object == self:
