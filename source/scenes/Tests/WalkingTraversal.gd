c353f330e739a9fc8cebdc04e83ba876cf268de8
nightblade9 <nightbladecodes@gmail.com>
2019-04-21 00:42:55 -0400

    Hindi language support. Keep a separate dictionary of fonts (`DynamicFont` saved as `tres`) to load at runtime

--- source/scenes/Tests/WalkingTraversal.gd
+++ source/scenes/Tests/WalkingTraversal.gd
@@ -1,9 +1,1 @@
 extends Spatial
-
-
-func _on_StaticBody_got_note(note):
-	# TODO: set content based on note
-	$NotePanel.visible = true
-	$NotePanel/Label.text = note.text
-	note.queue_free()
-	self.remove_child(note)
