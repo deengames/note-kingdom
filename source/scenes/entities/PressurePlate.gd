ff56099da8216f67b4029f04057717288d02b951
nightblade9 <nightbladecodes@gmail.com>
2019-04-25 19:31:35 -0400

    Pressure-plate audio

--- source/scenes/entities/PressurePlate.gd
+++ source/scenes/entities/PressurePlate.gd
@@ -10,10 +10,6 @@ var _is_player_touching = false
 
 export var auto_reset: = false # if this is on, it will untoggle itself if nothing is touching it 
 
-func _ready():
-	pass
-#	_OFF_ROTATION = $Handle.rotation_degrees
-
 func flip():
 	self._is_on = not self._is_on
 	if self._is_on:
@@ -22,26 +18,19 @@ func flip():
 	else:
 		$Plate.translation.y = 0
 #		$Handle.rotation_degrees = _OFF_ROTATION
+	$AudioStreamPlayer.play()
 
 	self._move_my_blocks()
 
 func _on_Area_body_entered(body):
 	if (body is Player or body is PushableBlock) and not _is_on:
-		print("is on is " + str(_is_on))
-#		_is_player_touching = true
 		self.flip()
-		
 
 func _on_Area_body_exited(body):
 	if body is Player or body is PushableBlock:
 #		_is_player_touching = false
 		if auto_reset and _is_on:
 			self.flip()
-
-#func _input(event):
-#    if "pressed" in event and not event.pressed and \
-#	_is_player_touching and event is InputEventKey and event.scancode == KEY_SPACE:
-#        self.flip()
 
 # Flips all the blocks in the same group in the parent as us.
 # Clever code. Find our parent, which is a group (eg. Node).
