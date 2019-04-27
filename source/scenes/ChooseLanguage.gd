69071dd6f9b28fe61ed018978aef5ef43f65dcf5
nightblade9 <nightbladecodes@gmail.com>
2019-04-26 21:28:33 -0400

    Exit button, settings button Language is now settable Refactored out settings panel component

--- source/scenes/ChooseLanguage.gd
+++ source/scenes/ChooseLanguage.gd
@@ -2,17 +2,29 @@ extends Panel
 
 signal picked_language
 
+const _language_arrow_x = {
+	"en-US": 380,
+	"ru-RU": 630,
+	"hi-HI": 880
+}
+
+func _ready():
+	_show_selected_language()
+
 func _on_EnglishLanguage_pressed():
 	Globals.set_language("en-US")
+	_show_selected_language()
 	emit_signal("picked_language")
-	queue_free()
 
 func _on_RussianLanguage_pressed():
 	Globals.set_language("ru-RU")
+	_show_selected_language()
 	emit_signal("picked_language")
-	queue_free()
 	
 func _on_HindiLanguage_pressed():
 	Globals.set_language("hi-HI")
+	_show_selected_language()
 	emit_signal("picked_language")
-	queue_free()
+	
+func _show_selected_language():
+	$Language/UpArrow.position.x = _language_arrow_x[Globals._language]
--- source/scenes/MainMenu.gd
+++ source/scenes/MainMenu.gd
@@ -1,14 +1,29 @@
 extends Node2D
 
-func _ready():
-	$ChooseLanguage.connect("picked_language", self, "_on_picked_language")
+const SettingsPanel = preload("res://scenes/SettingsPanel.tscn")
 
+var _button_names = {}
+
+func _ready():
+	# Persist button keys because we translate them
+	var buttons = [$ButtonsContainer/PlayButton, $ButtonsContainer/SettingsButton, $ButtonsContainer/CreditsButton, $ButtonsContainer/ExitButton]
+	for button in buttons:
+		_button_names[button] = button.text.to_upper().replace(" ", "_")
+		
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://scenes/WorldAreas/KingdomEntrance.tscn")
 
 func _on_picked_language():
 	var buttons = [$ButtonsContainer/PlayButton, $ButtonsContainer/SettingsButton, $ButtonsContainer/CreditsButton, $ButtonsContainer/ExitButton]
 	for button in buttons:
-		var key = button.text.to_upper().replace(" ", "_")
+		var key = _button_names[button]
 		button.add_font_override("font", Globals.get_language_font())
 		button.text = Globals.translate(key)
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
+
+func _on_SettingsButton_pressed():
+	var settings_panel = SettingsPanel.instance()
+	add_child(settings_panel)
+	settings_panel.connect("picked_language", self, "_on_picked_language")
