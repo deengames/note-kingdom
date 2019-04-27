6acabddfab9e5d6ea85a98b70d2e12cbf242317a
nightblade9 <nightbladecodes@gmail.com>
2019-04-26 23:28:54 -0400

    Partial mouse support for diary. Doesn't work.

--- source/scenes/GUI.gd
+++ source/scenes/GUI.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-var diary_panel = preload("res://scenes/DiaryPanel.tscn")
+var DiaryPanel = preload("res://scenes/DiaryPanel.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +10,18 @@ func _ready():
 func _process(delta):
 	# Chain of code to add or delete DiaryPanel
 	if Input.is_action_just_pressed("open_close_diary"):
-		# DiaryPanelContainer should store only "DiaryPanel" or do not store anything at all
-		var diary_panel_container_children = $DiaryPanelContainer.get_children()
-		# If DiaryPanelContainer already store something
-		if diary_panel_container_children != []:
-			diary_panel_container_children[0].queue_free()
-		else:
-			$DiaryPanelContainer.add_child(diary_panel.instance())
+		self._on_toggle_diary()
+
+func _on_toggle_diary():
+	# DiaryPanelContainer should store only "DiaryPanel" or do not store anything at all
+	var diary_panel_container_children = $DiaryPanelContainer.get_children()
+	# If DiaryPanelContainer already store something
+	if len(diary_panel_container_children) > 0:
+		diary_panel_container_children[0].queue_free()
+	else:
+		var panel = DiaryPanel.instance()
+		$DiaryPanelContainer.add_child(panel)
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton and event.pressed):
+		self._on_toggle_diary()
