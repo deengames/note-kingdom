57138dae4a9e605b0e51b13e3974755cfecde5e7
legendiguess <legendiguess@mail.ru>
2019-04-29 21:17:23 +0500

    moved save icon in gui and changed it activation from click to button

--- source/scenes/GUI.gd
+++ source/scenes/GUI.gd
@@ -12,6 +12,14 @@ func _process(delta):
 	# Chain of code to add or delete DiaryPanel
 	if Input.is_action_just_pressed("open_close_diary"):
 		self._on_toggle_diary()
+	if Input.is_action_just_pressed("save"):
+		$SaveGame.play()
+		var save_data = {
+			"last_room": Globals.last_room,
+			"current_room": Globals.current_room,
+			"notes_collected": Globals.notes_collected
+		}
+		SaveManager.save(SaveManager.SAVE_FILE_NAME, save_data)
 
 func _on_toggle_diary():
 	# DiaryPanelContainer should store only "DiaryPanel" or do not store anything at all
@@ -22,14 +30,3 @@ func _on_toggle_diary():
 	else:
 		var panel = DiaryPanel.instance()
 		$DiaryPanelContainer.add_child(panel)
-
-func _on_Area2D_input_event(viewport, event, shape_idx):
-	if (event is InputEventMouseButton and event.pressed):
-		# Save game
-		$SaveGame.play()
-		var save_data = {
-			"last_room": Globals.last_room,
-			"current_room": Globals.current_room,
-			"notes_collected": Globals.notes_collected
-		}
-		SaveManager.save(SaveManager.SAVE_FILE_NAME, save_data)
