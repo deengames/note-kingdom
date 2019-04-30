d9534682a4308ccd48cea001864ec0784bb08e51
nightblade9 <nightbladecodes@gmail.com>
2019-04-30 18:49:20 -0400

    Fix localization of credits Un-break main menu Credits button text

--- source/scenes/Credits.gd
+++ source/scenes/Credits.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 func _ready():
 	$Label.add_font_override("font", Globals.get_language_font())
-	$Label.text = Globals.translate("CREDITS").replace("@names", "Nightblade, mrdudeiii, legendiguess, Bhagat")
-
+	$Label.text = Globals.translate("CREDITS_TEXT").replace("@names", "Nightblade, mrdudeiii, legendiguess, Bhagat")
+	
+	$Button.add_font_override("font", Globals.get_language_font())
+	$Button.text = Globals.translate("BACK_TO_MAIN")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
